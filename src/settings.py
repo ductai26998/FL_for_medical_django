1fd0feeedf8aa68c454c2b3cccce9ecc668938f7
ductai26998 <ductai26998@gmail.com>
2022-12-04 19:20:03 +0700

    remove aws key

--- src/settings.py
+++ src/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # aws
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "AKIAYXJ7HLKL3LADNLFG")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "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")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
 FOLDER = os.getenv("TRAIN", "dev_train")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "fl-for-medical")
